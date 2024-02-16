94bd148d148bfd0475226a08da0dc7aefac09ec9
Dmitriy <dima.boyko.bd@gmail.com>
2024-02-16 21:45:06 +0200

    add draw-board-def

--- pacman.py
+++ pacman.py
@@ -94,6 +94,30 @@ dead_img = pygame.transform.scale(pygame.image.load("./Pacman Python/assets/ghos
 
 ghost_speeds = [2, 2, 2, 2] 
 
+# functions
+
+def draw_board():
+    for i in range(len(level)):
+        for j in range(len(level[i])):
+            if level[i][j] == 1:
+                pygame.draw.circle(screen, 'white', (j*num2 + (0.5*num2), i*num1 + (0.5*num1)), 4)
+            if level[i][j] == 2 and not flicker:
+                pygame.draw.circle(screen, 'white', (j*num2 + (0.5*num2), i*num1 + (0.5*num1)), 10)
+            if level[i][j] == 3:
+                pygame.draw.line(screen, color, (j*num2 + (0.5*num2), i*num1), (j*num2 + (0.5*num2), i*num1 + num1), 3)
+            if level[i][j] == 4:
+                pygame.draw.line(screen, color, (j*num2, i*num1 + (0.5*num1)), (j*num2 + num2, i*num1 + (0.5*num1)), 3)
+            if level[i][j] == 5:
+                pygame.draw.arc(screen, color, [(j*num2 - (num2*0.5)) + 2, (i*num1 + (num1*0.5)), num2, num1], 0, PI/2, 3)
+            if level[i][j] == 6:
+                pygame.draw.arc(screen, color, [(j*num2 + (num2*0.5)), (i*num1 + (num1*0.5)), num2, num1], PI/2, PI, 3)
+            if level[i][j] == 7:
+                pygame.draw.arc(screen, color, [(j*num2 + (num2*0.5)), (i*num1 - (num1*0.4)), num2, num1], PI, 3*PI/2, 3)
+            if level[i][j] == 8:
+                pygame.draw.arc(screen, color, [(j*num2 - (num2*0.4)) - 2, (i*num1 - (num1*0.4)), num2, num1], 3*PI/2, 2*PI, 3) 
+            if level[i][j] == 9:
+              
+
 # main game loop 
 
 run = True
