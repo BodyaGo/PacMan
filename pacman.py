bf341f55ad122d04dd0e844df93335262fb8d7a3
BodyaGo <mukhabodya@gmail.com>
2024-02-16 17:02:14 +0200

    first setup

---
+++ pacman.py
@@ -0,0 +1,40 @@
+import pygame
+import math
+from board import boards
+
+pygame.init()
+
+# settings variables: screen, framerates, fonts etc...
+
+# resolution, display
+WIDTH = 900
+HEIGHT = 950 
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+
+num1 = ((HEIGHT - 50) // 32 ) # column constant
+num2 = (WIDTH // 30) # row constant
+num3 = 15 # fat factor for the player
+font = pygame.font.Font('freesansbold.ttf', 20) # font: default windows
+level = boards # board array imported from board.py
+color = 'blue' # color variable
+PI = math.pi # pi 
+flicker = False # blinker variable
+turns_allowed = [False, False, False, False] # allowed way to turn
+score = 0 # score 
+powerup = False # state
+power_counter = 0 # timer
+eaten_ghost = [False, False, False, False] # ghost states
+moving = False # moving state
+startup_counter = 0 # counter
+lives = 3
+blinky_dead = False
+inky_dead = False
+pinky_dead = False
+clyde_dead = False
+blinky_box = False
+inky_box = False
+pinky_box = False
+clyde_box = False
+ghost_speed = 2
+game_over = False
+game_won = False
