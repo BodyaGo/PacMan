9578d84265593e93d294fd33560802d910663e28
Богдан Муха <mukha@MacBook-Air.local>
2024-02-16 16:10:24 +0200

    firs setup file

---
+++ pacman.py
@@ -0,0 +1,12 @@
+import pygame
+import math
+from board import boards
+
+pygame.init()
+
+# settings variables: screen, framerates, fonts etc...
+
+# resolution, display
+WIDTH = 900
+HEIGHT = 950 
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
