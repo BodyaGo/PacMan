e4defa8621c8c93b549d3632331a42a25ef0c6fa
Dmitriy <dima.boyko.bd@gmail.com>
2024-02-16 21:39:19 +0200

    main cycle

--- pacman.py
+++ pacman.py
@@ -44,4 +44,29 @@ player_x = 450
 player_y = 663
 player_speed = 2
 
-dima = 'vot moya nerealnaya variable' 
+# main game loop 
+
+run = True
+while run:
+    timer.tick(fps)
+    if counter < 19:
+        counter += 1
+        if counter > 3:
+            flicker = False
+    else:
+        counter = 0
+        flicker = True
+    if powerup and power_counter < 600:
+        power_counter += 1
+    elif powerup and power_counter >= 600:
+        power_counter = 0
+        powerup = False
+        eaten_ghost = [False, False, False, False]
+    if startup_counter < 180 and not game_over and not game_won:
+        moving = False
+        startup_counter += 1
+    else:
+        moving = True
+
+    pygame.display.flip()
+pygame.quit()
