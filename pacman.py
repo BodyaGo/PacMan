f076acc2f9278cd177d63ed9ac93dcbe3b4b5b4d
BebraUser <your email address kolyanhidan10@gmail.com>
2024-02-17 19:25:15 +0200

    added draw func

--- pacman.py
+++ pacman.py
@@ -63,7 +63,14 @@ class Ghost:
         self.rect = self.draw()
 
     def draw(self):
-        pass
+        if (not powerup and not self.dead) or (eaten_ghost[self.id] and powerup and not self.dead):
+            screen.blit(self.img, (self.x_pos, self.y_pos))
+        elif powerup and not self.dead and not eaten_ghost[self.id]:
+            screen.blit(spooked_img, (self.x_pos, self.y_pos))
+        else:
+            screen.blit(dead_img, (self.x_pos, self.y_pos))
+        ghost_rect = pygame.rect.Rect((self.center_x - 18, self.center_y - 18), (36, 36))
+        return ghost_rect
 
     def check_collisions(self):
         pass
