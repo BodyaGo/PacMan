4cdf6617758a4b4707352d29cc5b2533e80b088d
Kolya Kodenov <kolya_kodenov@knu.ua>
2024-02-17 21:16:14 +0200

    added draw_player

--- pacman.py
+++ pacman.py
@@ -658,22 +658,22 @@ class Ghost:
         screen.blit(gameover_text, (100, 300))
     
     def check_collisions(scor, power, power_count, eaten_ghosts):
-    num1 = (HEIGHT - 50) // 32
+        num1 = (HEIGHT - 50) // 32
     num2 = WIDTH // 30
     if 0 < player_x < 870:
         if level[center_y // num1][center_x // num2] == 1:
             level[center_y // num1][center_x // num2] = 0
-            scor += 10
+            score += 10
         if level[center_y // num1][center_x // num2] == 2:
             level[center_y // num1][center_x // num2] = 0
-            scor += 50
+            score += 50
             power = True
             power_count = 0
             eaten_ghosts = [False, False, False, False]
-    return scor, power, power_count, eaten_ghosts
+    return score, power, power_count, eaten_ghosts
 
     def draw_board():
-    num1 = ((HEIGHT - 50) // 32)
+        num1 = ((HEIGHT - 50) // 32)
     num2 = (WIDTH // 30)
     for i in range(len(level)):
         for j in range(len(level[i])):
@@ -703,3 +703,13 @@ class Ghost:
             if level[i][j] == 9:
                 pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
                                  (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+    def draw_player():
+    # 0-RIGHT, 1-LEFT, 2-UP, 3-DOWN
+        if direction == 0:
+         screen.blit(player_images[counter // 5], (player_x, player_y))
+        elif direction == 1:
+            screen.blit(pygame.transform.flip(player_images[counter // 5], True, False), (player_x, player_y))
+        elif direction == 2:
+            screen.blit(pygame.transform.rotate(player_images[counter // 5], 90), (player_x, player_y))
+        elif direction == 3:
+            screen.blit(pygame.transform.rotate(player_images[counter // 5], 270), (player_x, player_y))
