5d39f416187e117a55f1a0e1a22e7aa58cd7989f
BodyaGo <mukhabodya@gmail.com>
2024-02-20 20:34:35 +0200

    added comments

--- pacman.py
+++ pacman.py
@@ -98,6 +98,7 @@ ghost_speeds = [2, 2, 2, 2]
 
 class Ghost:
     def __init__(self, x_coord, y_coord, target, speed, img, direct, dead, box, id):
+        # Ініціалізація параметрів привида
         self.x_pos = x_coord
         self.y_pos = y_coord
         self.center_x = self.x_pos + 22
@@ -113,17 +114,20 @@ class Ghost:
         self.rect = self.draw()
 
     def draw(self):
+        # Малювання привида на екрані
         if (not powerup and not self.dead) or (eaten_ghost[self.id] and powerup and not self.dead):
             screen.blit(self.img, (self.x_pos, self.y_pos))
         elif powerup and not self.dead and not eaten_ghost[self.id]:
             screen.blit(spooked_img, (self.x_pos, self.y_pos))
         else:
             screen.blit(dead_img, (self.x_pos, self.y_pos))
+        # Створення прямокутника для обробки колізій
         ghost_rect = pygame.rect.Rect((self.center_x - 18, self.center_y - 18), (36, 36))
         return ghost_rect
 
     def check_collisions(self):
         # R, L, U, D
+        # Перевірка колізій привида з оточуючим середовищем
         num1 = ((HEIGHT - 50) // 32)
         num2 = (WIDTH // 30)
         num3 = 15
